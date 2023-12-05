0559d646d765591495e52a86e4c8c04506e9629e
smitt0101 <smitkalkani9099@gmail.com>
2023-12-05 13:40:48 -0500

    Smit Todo Backend Service API developed

--- src/main/kotlin/com/smittodobackend/data/KmongoClient.kt
+++ src/main/kotlin/com/smittodobackend/data/KmongoClient.kt
@@ -5,6 +5,6 @@ import com.smittodobackend.model.Task
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
-val kmongoClient = KMongo.createClient(ConnectionString("mongodb://mongo:27017")).coroutine
+val kmongoClient = KMongo.createClient(ConnectionString("mongodb+srv://smit:9099850857@cluster0.srki2ok.mongodb.net/?retryWrites=true&w=majority")).coroutine
 val taskDatabase = kmongoClient.getDatabase("Tasks")
 val taskCollection = taskDatabase.getCollection<Task>()
