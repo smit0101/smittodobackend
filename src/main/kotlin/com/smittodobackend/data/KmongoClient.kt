165ac7db36e4c441db1e20f5d9a55fe552320326
Smit kalkani <63602221+smit0101@users.noreply.github.com>
2023-12-14 08:11:43 -0500

    Update KmongoClient.kt

--- src/main/kotlin/com/smittodobackend/data/KmongoClient.kt
+++ src/main/kotlin/com/smittodobackend/data/KmongoClient.kt
@@ -6,6 +6,6 @@ import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
 //val kmongoClient = KMongo.createClient(ConnectionString("mongodb+srv://smit:9099850857@cluster0.srki2ok.mongodb.net/?retryWrites=true&w=majority")).coroutine
-val kmongoClient = KMongo.createClient(ConnectionString("mongodb://mongodb:27017")).coroutine
+val kmongoClient = KMongo.createClient(ConnectionString("mongodb://localhost:27017")).coroutine
 val taskDatabase = kmongoClient.getDatabase("Tasks")
 val taskCollection = taskDatabase.getCollection<Task>()
